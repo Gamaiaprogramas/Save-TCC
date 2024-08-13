307b5b33b159de9ca905276bc2878329198a3e5d
Gamaia <gamaiagamer@outlook.com>
2024-08-13 14:40:18 -0300

    commit

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,90 @@
-
+<footer class="footer">
+        <div class="container2">
+            <div class="col1">
+                <a href="#" class="brand">Redes Sociais</a>
+                <ul class="media-icons">
+                    <li>
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-steam"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col2">
+                <ul class="menu2">
+                    <li>
+                        <a href="#">inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">contato</a>
+                    </li>
+                    <li>
+                        <a href="#">blog</a>
+                    </li>
+                    <li>
+                        <a href="#">serviços</a>
+                    </li>
+                </ul>
+                <p class="info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ratione ea quo esse blanditiis et molestias error ad perferendis dignissimos, iste deserunt nobis aut cupiditate facere, reiciendis incidunt minus laudantium.
+                </p>
+                <p class="copy">
+                 <strong>Copyright 2024 | Yasuo Gym - Todos os direitos Reservados</strong>
+                </p>
+            </div>
+            <div class="col3">
+                <p>Se inscreva para novidades</p>
+                <form action="">
+                    <div class="input-wrap">
+                        <input type="text" name="" id="txt" placeholder="exemplo@gmail.com">
+                        <button type="submit">
+                            <i class="fas fa-paper-plane"></i>
+                        </button>
+                    </div>
+                </form>
+                <ul class="services-icons">
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-amazon-pay"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-mastercard"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-apple-pay"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-stripe"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-visa"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+ 
 </body>
 </html>
