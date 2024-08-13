10abaa3625bd028fd3e2d2d3700977817066f262
Gamaia <gamaiagamer@outlook.com>
2024-08-13 14:54:10 -0300

    Guilherme/Alison - 13/08

--- PAGES/landing.php
+++ PAGES/landing.php
@@ -26,15 +26,15 @@
     </button>
   </div>
 
-  <div class="QuemSomos">
+  <div class="quemSomos">
       <h1>Quem somos?</h1>
-      <div class="QuemDiv1">
+      <div>
         <img src="../PICS/logo-corte.png">
         <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae similique ipsam iste veritatis voluptate eum, optio nisi enim aliquam in dicta dolorum qui, ipsum expedita exercitationem facilis, fuga ex! Repudiandae.</h1>
       </div>
-      <div class="QuemDiv2">
-        <img src="../PICS/logo-corte.png">
+      <div >
         <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga animi voluptatem numquam nihil quam enim, iusto tempora aperiam fugiat illo blanditiis dignissimos dolorem architecto, deserunt dolores rerum! Minima, alias ea.</h1>
+        <img src="../PICS/logo-corte.png">
       </div>
   </div>
 
