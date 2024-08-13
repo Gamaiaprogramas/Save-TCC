4f3237cb7e9fd5841a3bfb33b8c550e0f6dd6416
Takara06 <kenititakara@gmail.com>
2024-08-13 14:49:28 -0300

    Filipe -- 13/08 -- fotos

--- PAGES/landing.php
+++ PAGES/landing.php
@@ -7,13 +7,13 @@
   <div id="carouselExampleIndicators" class="carousel slide">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="../PICS/carrossel1.jpg"class ="d-block w-100" alt="...">
+        <img src="../PICS/fotos_teste/recuperacao-financeira-de-coronavirus_23-2148549657.jpg"class ="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="../PICS/carrossel2.jpg" class="d-block w-100" alt="...">
+        <img src="../PICS/fotos_teste/mulher-em-pilhas-de-moedas-com-seta-para-cima-infografico_24877-63697.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="../PICS/carrossel3.jpg" class="d-block w-100" alt="...">
+        <img src="../PICS/fotos_teste/renderizacao-3d-de-fundo-de-negocios_23-2150563473.jpg" class="d-block w-100" alt="...">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
