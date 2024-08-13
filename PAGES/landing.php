896ebcc13d9f524f68e3951fb6536c61815318e3
Gamaia <gamaiagamer@outlook.com>
2024-08-13 14:09:26 -0300

    correcao

--- PAGES/landing.php
+++ PAGES/landing.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="PT-BR">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <title>Carrossel de Teste</title>
-  <link rel="stylesheet" href="/STYLE/landing.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
+
   <?php
     include("../partials/header.php");
   ?>
@@ -35,5 +25,9 @@
       <span class="visually-hidden">Next</span>
     </button>
   </div>
-</body>
-</html>
+
+<?php 
+
+  include("../partials/footer.php")
+
+?>
