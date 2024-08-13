4f3a052ea13f770067753a8bf1133e5d540af41a
luis henrique <lourencoluisoppx22@gmail.com>
2024-08-13 14:32:51 -0300

    luis -- 13/08 -- footer

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Save-TCC
 
-https://prod.liveshare.vsengsaas.visualstudio.com/join?4EB75827D0AD267735079800A7541F1E83F6
--- partials/header.php
+++ partials/header.php
@@ -6,10 +6,11 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <title>Carrossel de Teste</title>
   <link rel="stylesheet" href="../STYLE/landing.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-<nav>
+    <nav>
         <div class="frst">
             <ul class="menu">
                 <li><a href="#">Suporte</a></li>
@@ -30,4 +31,92 @@
             </ul>
         </div>
     </nav>
+
+    <footer class="footer">
+        <div class="container2">
+            <div class="col1">
+                <a href="#" class="brand">Redes Sociais</a>
+                <ul class="media-icons">
+                    <li>
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-steam"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col2">
+                <ul class="menu2">
+                    <li>
+                        <a href="#">inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">contato</a>
+                    </li>
+                    <li>
+                        <a href="#">blog</a>
+                    </li>
+                    <li>
+                        <a href="#">serviços</a>
+                    </li>
+                </ul>
+                <p class="info">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ratione ea quo esse blanditiis et molestias error ad perferendis dignissimos, iste deserunt nobis aut cupiditate facere, reiciendis incidunt minus laudantium.
+                </p>
+                <p class="copy">
+                 <strong>Copyright 2024 | Yasuo Gym - Todos os direitos Reservados</strong>
+                </p>
+            </div>
+            <div class="col3">
+                <p>Se inscreva para novidades</p>
+                <form action="">
+                    <div class="input-wrap">
+                        <input type="text" name="" id="txt" placeholder="exemplo@gmail.com">
+                        <button type="submit">
+                            <i class="fas fa-paper-plane"></i>
+                        </button>
+                    </div>
+                </form>
+                <ul class="services-icons">
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-amazon-pay"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-mastercard"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-apple-pay"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-stripe"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-cc-visa"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
  
