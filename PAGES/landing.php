64dfca23f3fdcabfabdb7f29c52d3d3431a1e799
Gamaia <gamaiagamer@outlook.com>
2024-08-27 17:32:10 -0300

    Semi-Organizcao

--- PAGES/landing.php
+++ PAGES/landing.php
@@ -1,96 +1,231 @@
-  <link rel="stylesheet" href="../STYLE/landing.css">
-  <?php
-    include("../partials/header.php");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../STYLE/landing.css">
+    <script src="functions.js"></script>
+    <script src="https://kit.fontawesome.com/5e3d9cd23b.js" crossorigin="anonymous"></script>
+    <title>Save your money</title>
+</head>
+<body>
+    <!--header-->
+    <header id="header">
 
-  ?>
-  <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-  <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
-  <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!--container-->
+        <div class="container">
 
+            <!--flex-->
+            <div class="flex">
 
-  <div id="carouselExampleIndicators" class="carousel slide">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="../PICS/fotos_teste/recuperacao-financeira-de-coronavirus_23-2148549657.jpg"class ="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="../PICS/fotos_teste/mulher-em-pilhas-de-moedas-com-seta-para-cima-infografico_24877-63697.jpg" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="../PICS/fotos_teste/renderizacao-3d-de-fundo-de-negocios_23-2150563473.jpg" class="d-block w-100" alt="...">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
+                <a href="index.html">
+                    <img src="../PICS/img2/logo2-removebg-preview.png" alt="Logo">
+                </a>
+                
+                    <nav>
+                        <ul class="ul">
+                            <a href="">Home</a>
+                            <a href="">Sobre</a>
+                            <a href="">Serviços</a>
+                            
+                        </ul>
+                   
+                    </nav>
 
-  <div class="quemSomos">
-      <h1>Quem somos?</h1>
-      <div>
-        <img src="../PICS/logo-corte.png">
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae similique ipsam iste veritatis voluptate eum, optio nisi enim aliquam in dicta dolorum qui, ipsum expedita exercitationem facilis, fuga ex! Repudiandae.</h1>
-      </div>
-      <div >
-        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga animi voluptatem numquam nihil quam enim, iusto tempora aperiam fugiat illo blanditiis dignissimos dolorem architecto, deserunt dolores rerum! Minima, alias ea.</h1>
-        <img src="../PICS/logo-corte.png">
-      </div>
-  </div>
-
-    <div class="servicos">
-      <h1>Serviços</h1>
-      <div>
-        <!-- primeira div -->
-        <div>
-          <div>
-            <h1>Dê uma chance ao conhecer!</h1>
-            <img src="">
-            <input type="button" value="Aprenda">
-          </div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe placeat unde quis at officiis laboriosam magni repudiandae possimus ad ipsam nisi facere corporis molestias ipsa distinctio quidem praesentium, officia debitis?</p>
-        </div>
         
-         <!-- segunda div -->
-        <div>
-          <div>
-          <h1>Dê uma chance ao conhecer!</h1>
-          <img src="">
-          <input type="button" value="Aprenda">
+                    <div class="btn-contato">
+                           <a class="btn" href="">Contato</a>
+                    </div> 
+                   
+            
+                <div class="menu-icon">
+                    <i class="fa-solid fa-bars"></i>
+                </div>
+                
+
+            </div>
+            <!--end flex-->
+
         </div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam laboriosam et veniam cupiditate ratione minima repellat harum autem, illo est. Minima deserunt sequi expedita perspiciatis provident! Consequatur dolor incidunt similique!</p>
+        <!--end container-->
+
+    </header>
+    <!--end header-->
+
+    <!--Banner-->
+    <section class="banner" style="background-image: url(../PICS/img2/background-home2.png);">
+        <div class="banner-text">
+            <h1>Save My Money</h1>
+            <p>"Estamos aqui para ajudar, vamos planejar a melhor solução para você".</p>
+            <a class="btn animate-btn">Entrar em Contato</a>
         </div>
+    </section>
+     <!--end Banner-->
 
-         <!-- terceira div -->
-        <div>
-        <div>
-          <h1>Dê uma chance ao conhecer!</h1>
-          <img src="">
-          <input type="button" value="Aprenda">
+    <!--faca mais-->
+    <section class="faca-mais">
+        <div class="container">
+            <div class="faca-mais-txt">
+                <h3 id="sobre">Nossa empresa</h3>
+                <p>O basquete foi inventado em dezembro de <b>1891</b> pelo professor de educação física <b>James Naismith</b>, em Springfield, Massachusetts, EUA. Naismith foi desafiado
+                     a criar um novo esporte indoor para manter seus alunos ativos durante o inverno. Ele elaborou um jogo com 13 regras básicas, usando uma bola de futebol e cestos de pêssego como gols, pendurados em cada extremidade do ginásio. A ideia era marcar pontos lançando a bola nos cestos.
+                     O jogo evoluiu com o tempo, e as regras foram ajustadas, mas a essência do basquete permaneceu. O esporte
+                     rapidamente se espalhou pelos Estados Unidos e pelo mundo, tornando-se um dos esportes mais populares e praticados globalmente.</p>
+            </div>
+            <div class="faca-mais-img">
+                <img src="../PICS/img2/4374615.jpg" alt="" >
+            </div>
         </div>
+            
+    </section>
+    <!--end faca mais-->
+    <!--faca mais2-->
+    <section class="faca-mais">
+        <div class="container">
+            <div class="faca-mais-img">
+                <img src="../PICS/img2/4714191.jpg" alt="">
+            </div>
+            <div class="faca-mais-txt">
+                <h3>Como Lidamos Com Dinheiro</h3>
+                <br>
+                <p>O dinheiro é uma ferramenta poderosa que molda muitos aspectos da 
+                    nossa vida cotidiana. Ele pode abrir portas para oportunidades e proporcionar conforto, mas também pode gerar stress 
+                    e desigualdades. Embora a busca por riqueza possa levar a conquistas significativas, é importante lembrar que o verdadeiro valor não está 
+                    apenas na quantidade acumulada, mas na forma como usamos nossos recursos para criar impacto positivo. Investir em experiências, compartilhar com os outros e buscar 
+                    equilíbrio pode transformar o dinheiro de um simples meio de troca em um catalisador para uma vida mais rica e significativa.
+                </p>
+            </div>
+            
+        </div>       
+    </section>
+     <!--end faca mais2-->
+    <!--faca mais3-->
+    <section class="faca-mais">
+        <div class="container">
+            <div class="faca-mais-txt">
+                <h3>Educação Financeira</h3>
+                <p>A educação financeira é essencial para alcançar estabilidade e prosperidade econômica. Ela envolve aprender a gerenciar recursos de maneira eficaz, entender conceitos básicos como orçamento, poupança, investimentos e dívidas, e desenvolver habilidades para tomar decisões financeiras informadas.
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rem quam placeat praesentium non, vitae fugit sunt libero blanditiis sed minima inventore facilis cupiditate vel pariatur soluta neque magnam est.</p>
-      </div>
+                    Com uma boa educação financeira, é possível:
+                    
+                    Planejar e Orçar: Criar um orçamento permite monitorar receitas e despesas, ajudando a identificar áreas onde é possível economizar e evitar gastos excessivos.
+                    
+                    Economizar e Investir: Aprender a poupar e investir de forma inteligente pode ajudar a construir um patrimônio e preparar-se para imprevistos e objetivos futuros, como a aposentadoria.
+                    
+                    Gerenciar Dívidas: Compreender as diferentes formas de dívida e como gerenciá-las eficazmente pode prevenir problemas financeiros graves e reduzir o estresse associado às dívidas.
+                    
+                    Proteger-se Financeiramente: Conhecer os tipos de seguros e estratégias de proteção financeira ajuda a minimizar riscos e a garantir uma maior segurança econômica.
+                    
+                    Tomar Decisões Informadas: A educação financeira capacita você a tomar decisões mais assertivas sobre compras, empréstimos e investimentos, promovendo uma vida financeira mais equilibrada.
+                </p>
+            </div>
+            <div class="faca-mais-img">
+                <img src="../PICS/img2/9796295.jpg" alt="">
+            </div>
+        </div>       
+    </section>
+     <!--end faca mais3-->
+    <!--Depoimentos-->
+    <section class="depoimentos">
+        <h3>Criadores</h3>
+        <p>Nosso time de Devs:</p>
+        <div class="cards">
+            <!--card1-->
+            <div class="card-item">
+                <img src="../PICS/img2/jordan.png" alt="Michael Jordan">
+                <p class="nome-user">Guilherme</p>
+                <p class="depoimento-user">Michael Jordan é amplamente considerado um dos maiores jogadores de basquete de todos os tempos.
+                     Nascido em 1963, ele teve uma carreira notável na NBA, principalmente com o Chicago Bulls, com quem conquistou
+                      seis títulos de campeão (1991-1993, 1996-1998). Jordan é conhecido por suas habilidades excepcionais, incluindo seu famoso salto e a habilidade de marcar pontos em momentos decisivos.
+                     Ele ganhou cinco prêmios de MVP da temporada regular e dois prêmios de MVP das Finais da NBA, além de ser um ícone global do esporte e um influente empresário.
+                </p>
+            </div> 
+             <!--end card1-->     
+            <!--card2-->
+            <div class="card-item">
+                <img src="../PICS/img2/LeBron.jpg" alt="Lebron James">
+                <p class="nome-user">Luis</p>
+                <p class="depoimento-user">LeBron James é um dos maiores jogadores de basquete da atualidade. Nascido em 1984, ele iniciou sua carreira na NBA com o Cleveland Cavaliers e, 
+                    desde então, jogou também pelo Miami Heat, Cleveland Cavaliers novamente e Los Angeles Lakers. LeBron é conhecido por sua versatilidade, habilidades excepcionais e 
+                    liderança em quadra, conquistando vários títulos de MVP da temporada e das Finais da NBA, além de quatro campeonatos. Ele também é um influente ativista e empresário.
+                </p>
+            </div> 
+            <!--end card2-->     
+            <!--card3-->
+            <div class="card-item">
+                <img src="../PICS/img2/kobe.png" alt="Kobe Bryant">
+                <p class="nome-user">Alison</p>
+                <p class="depoimento-user">Kobe Bryant, nascido em 1978 e falecido em 2020, foi um dos maiores jogadores de basquete 
+                    da NBA. Passou toda a sua carreira de 20 anos com o Los Angeles Lakers, conquistando cinco títulos de campeão (2000-2002, 2009-2010) e dois prêmios de MVP das Finais da NBA. Conhecido por sua ética de trabalho intensa e habilidades excepcionais, Kobe também ganhou o prêmio de MVP da temporada regular em
+                     2008 e foi 18 vezes All-Star. Além de suas conquistas em quadra, ele se destacou como empresário e filantropo, influenciando o esporte e a cultura globalmente.
+                </p>
+            </div> 
+             <!--end card3-->     
+            <!--card4-->
+            <div class="card-item">
+                <img src="../PICS/img2/curry.png" alt="Stephen Curry">
+                <p class="nome-user">Felipe</p>
+                <p class="depoimento-user">Stephen Curry, nascido em 1988, é um dos maiores jogadores de basquete da NBA e é 
+                    amplamente conhecido por sua habilidade excepcional em arremessos de longa distância. Atuando pelo Golden State Warriors desde 2009, Curry revolucionou o jogo com sua precisão de três pontos e seu estilo de jogo dinâmico. Ele conquistou quatro títulos da NBA
+                     (2015, 2017, 2018, 2022) e dois prêmios de MVP da temporada regular (2015, 2016). Curry é amplamente reconhecido por popularizar o arremesso de três pontos e por seu impacto tanto dentro quanto fora das quadras.
+                </p>
+            </div> 
+            <!--end card4-->        
+            <!--card5-->
+            <div class="card-item">
+                <img src="../PICS/img2/Oscar.png" alt="Oscar Schmidt">
+                <p class="nome-user">Alisson</p>
+                <p class="depoimento-user">Oscar Schmidt, nascido em 1958 no Brasil, é um dos maiores jogadores de basquete da história do país. Conhecido como
+                     "Mão Santa" por sua habilidade excepcional em arremessos, Schmidt é o maior pontuador da história do basquete internacional, acumulando mais de 49.000 pontos ao longo de sua carreira. Ele teve uma carreira de sucesso jogando
+                      na Liga Nacional de Basquete do Brasil e na Europa, especialmente pelo time italiano Caserta. Schmidt também teve destaque
+                     na seleção brasileira, com a qual ganhou a medalha de ouro nos Jogos Pan-americanos de 1987. Embora não tenha jogado na NBA, sua influência no basquete global é inegável.
+                </p>
+            </div>
+            <!--end card5-->
+        </div>              
+    </section> 
+    <!--END Depoimentos--> 
+    <!--Clientes-->
+    <section class="clientes">
+        <div class="container">
+            <h3 id="campeonatos">Nossos Parceiros</h3>
+            <p>Alguns de nossos parceiros de mercado</p>
+            <div>
+                <img src="../PICS/img2/apple.png" alt="">
+                <img src="../PICS/img2/google.png" alt="">
+                <img src="../PICS/img2/microsoft.png" alt="">
+                <img src="../PICS/img2/slack.png" alt="">
+                <img src="../PICS/img2/cloud.png" alt="">
+                
+            </div>
+        </div>
+    </section>  
+    <!--end Clientes-->
+     <!--faca mais4-->
+     <section class="faca-mais">
+        <div class="container">
+            <div class="faca-mais-img">
+                <img src="../PICS/img2/6811531.jpg" alt="">
+            </div>
+            <div class="faca-mais-txt">
+                <h3>Qualquer Titulo</h3>
+                <p>As dívidas são uma realidade complexa que afeta muitas pessoas. Elas podem surgir de diversas formas, 
+                    como empréstimos, cartões de crédito ou financiamentos, e muitas vezes se acumulam quando há um descompasso entre a renda e os gastos. Embora possam 
+                    oferecer soluções temporárias para necessidades urgentes, a acumulação de dívidas pode levar a um ciclo de estresse e preocupação.</p>
+            </div>
+        </div>       
+    </section>
+    <script>
 
-      </div>
-    </div>
-    <div class="nossoDiferencial">
-        <h1>Nosso Diferencial</h1>
-        <div>
-          
-        <div id="chartdiv"></div>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo, ullam laudantium ab mollitia iusto repellendus iure unde fuga quas vel itaque quod repellat sint quam dolore libero aperiam earum!</h2>
-    </div></div>
-    
-<?php 
+window.addEventListener("scroll",function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem',window.scrollY > 250)
+})
+</script>
+     <!--end faca mais4-->
+    <!--footer-->
+<?php
 
-  include("../partials/footer.php")
+include("./partials/footer.php");
 
 ?>
-<script> window.onload = LandingAct(); </script>
