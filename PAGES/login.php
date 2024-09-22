8d63a9eccd9f8dfd930e02a2d09c414823db1b2b
Alisson <alissonantony12@gmail.com>
2024-09-21 22:54:14 -0300

    .

--- PAGES/login.php
+++ PAGES/login.php
@@ -1,26 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../STYLE/cadastro.css">
+    <title>Cadastro</title>
+</head>
 <body>
-        <?php
-    @session_start();
-    if (isset($_SESSION['msg'])) {
-        echo $_SESSION['msg'];
-        unset($_SESSION['msg']);
-    }
-    ?>
-<div class="containerForm">
- 
-        <div class="log">
-        <form action="../ACTS/login.act.php" method="post" enctype="multipart/form-data" id="formAddCliente" onsubmit="return verificaForm()">
-        <h1>login</h1>
-        <label><p>Email:</p><input type="text" name="email"></label>
-        <label><p>Senha:</p><input type="passworld" name="senha1" onkeyup="verificaSenha(senha1.value,senha2.value)" require></label>
-        <label><input type="submit" value="Logar" id = "lug"></label>
-        </form>
-         </div>
 
-</div>
-
+    <div class="main-login">
+        <div class="left-login">
+            <h1> <span>Faça login </span> <br>Para começar sua jornada</h1>
+            <img src="../PICS/imgs/manage-money-animate.svg" class = "left-cadastro-image" alt="" srcset="">
+        </div>
+        <div class="right-login">
+            <div class="card-login">
+                <h1>Entrar na <span>Save</span> </h1>
+                <div class="textfield">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" placeholder="Email">
+                </div>
+                <div class="textfield">
+                    <label for="senha">Senha</label>
+                    <input type="text" name="senha" placeholder="Senha  ">
+                </div>
+                <div class="textfield">
+                    <label for="confirmar-senha">Confirmar senha</label>
+                    <input type="text" name="confirmar-senha" placeholder="Confirmar senha">
+                </div>
+                <button class="btn-cadastrar-se">Entrar</button>
+            </div>
+        </div>
+    </div>
+    
 </body>
-
-
-
-<?php include('../partials/footer.php'); ?>
+</html>
