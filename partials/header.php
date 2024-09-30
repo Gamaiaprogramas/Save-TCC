4efe774e24595eb5b9b90016dc749fabf64d1d8f
luis henrique <lourencoluisoppx22@gmail.com>
2024-09-30 17:35:17 -0300

    Alison Arruda 30-09

--- partials/header.php
+++ partials/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <link rel="stylesheet" href="../STYLE/header.css">
     <link rel="stylesheet" href="../STYLE/responsive.css">
     <link rel="shortcut icon" href="../PICS/img2/logo.png" type="image/x-icon">
