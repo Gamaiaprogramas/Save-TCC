1197e4b6289a789e902ff60543c6f70507428ba6
Alison Oliveira <alisonoliveira0116@gmail.com>
2024-10-23 16:01:47 -0300

    Alison Arruda -- 23/10

---
+++ PAGES/edit.php
@@ -0,0 +1,17 @@
+<?php 
+
+    include("../partials/header.php");
+
+?>
+
+<link rel="stylesheet" href="../STYLE/landing.css">
+
+<style>
+  header {
+    position: relative !important;
+    background-color: #10002b !important;
+    height: 7vw !important;
+    margin: 0 !important;
+    z-index: 1;
+  }
+</style>
