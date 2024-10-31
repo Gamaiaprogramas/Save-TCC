d48e69b87bf9ae94f202234655ad133cc12ff578
Takara06 <Kenititakara@gmail.com>
2024-10-31 17:44:16 -0300

    luis -- 31/10

--- PAGES/landing.php
+++ PAGES/landing.php
@@ -1,5 +1,12 @@
 <?php 
 
+    @session_start();
+    if (isset($_SESSION['msg'])) {
+        echo $_SESSION['msg'];
+        unset($_SESSION['msg']);
+    }
+
+
     include("../partials/header.php");
 
 ?>
