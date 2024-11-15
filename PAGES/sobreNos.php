5ab957355e19860ab41a5c5d74d8c3d7e1d5606b
Gamaia <gamaiagamer@outlook.com>
2024-11-15 17:59:32 -0300

    Guilherme Marques

--- PAGES/sobreNos.php
+++ PAGES/sobreNos.php
@@ -8,6 +8,9 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../STYLE/sobreNos.css">
+    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Save - Sobre nós</title>
 </head>
@@ -42,7 +45,8 @@
         </div>
     </div>
     <div class="espacoGrafico">
-
+        <div class = "textoGrafico"><div></div><h1>Percentual de famílias endividadas (de acordo com a CNC): </h1></div>
+    <div id="chartdiv" style = "height : 100%; width : 100%;"></div>
     </div>
     <div class="espacoOQ">
         <div class="tituloOQ">
@@ -135,4 +139,127 @@
         </div>
     </div>
 </body>
+<script>
+am5.ready(function() {
+
+// Create root element
+// https://www.amcharts.com/docs/v5/getting-started/#Root_element
+var root = am5.Root.new("chartdiv");
+
+// Set themes
+// https://www.amcharts.com/docs/v5/concepts/themes/
+root.setThemes([
+  am5themes_Animated.new(root)
+]);
+
+// Create chart
+// https://www.amcharts.com/docs/v5/charts/xy-chart/
+var chart = root.container.children.push(am5xy.XYChart.new(root, {
+
+ 
+  
+  paddingLeft:0,
+  paddingRight:1
+}));
+
+// Add cursor
+// https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+cursor.lineY.set("visible", false);
+
+
+// Create axes
+// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+var xRenderer = am5xy.AxisRendererX.new(root, { 
+  minGridDistance: 30, 
+  minorGridEnabled: true
+});
+
+xRenderer.labels.template.setAll({
+  rotation: 0, // Define a rotação como 0 para deixar os rótulos na horizontal
+  centerY: am5.p50,
+  centerX: am5.p100,
+  paddingRight: 15
+});
+
+
+xRenderer.grid.template.setAll({
+  location: 1
+})
+
+var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+  maxDeviation: 0.3,
+  categoryField: "country",
+  renderer: xRenderer,
+  tooltip: am5.Tooltip.new(root, {})
+}));
+
+var yRenderer = am5xy.AxisRendererY.new(root, {
+  strokeOpacity: 0.1
+})
+
+var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+  maxDeviation: 0.3,
+  renderer: yRenderer,
+  min: 0,  // Valor mínimo do eixo Y
+  max: 100   //
+}));
+
+// Create series
+// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+  name: "Series 1",
+  xAxis: xAxis,
+  yAxis: yAxis,
+  valueYField: "value",
+  sequencedInterpolation: true,
+  categoryXField: "country",
+  tooltip: am5.Tooltip.new(root, {
+    labelText: "{valueY}"
+  })
+}));
+
+series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5, strokeOpacity: 0 });
+series.columns.template.adapters.add("fill", function (fill, target) {
+  return "#ff6d00";  // Define a cor das colunas para #ff6d00
+});
+
+series.columns.template.adapters.add("stroke", function (stroke, target) {
+  return "#ff6d00";  // Define a cor do contorno das colunas para #ff6d00
+});
+
+
+
+// Set data
+var data = [{
+  country: "2019",
+  value: 60
+}, {
+  country: "2020",
+  value: 67
+}, {
+  country: "2021",
+  value: 71
+}, {
+  country: "2022",
+  value: 78
+}, {
+  country: "2023",
+  value: 81
+}, {
+  country: "2024",
+  value: 80
+}];
+
+xAxis.data.setAll(data);
+series.data.setAll(data);
+
+
+// Make stuff animate on load
+// https://www.amcharts.com/docs/v5/concepts/animations/
+series.appear(1000);
+chart.appear(1000, 100);
+
+}); // end am5.ready()
+</script>
 </html>
