03efb7a91fa387bf9b5bb299486f9c9d4759e38b
Alison Arruda <alisonoliveira0116@gmail.com>
2024-11-17 04:31:49 -0300

    Alison Arruda - 17-11

--- PAGES/sobreNos.php
+++ PAGES/sobreNos.php
@@ -40,13 +40,15 @@
             <a>Antes de tudo</a>
         </div>
         <div class="textoAntes">
-            <a>Em 2024, o endividamento das famílias brasileiras continua elevado, principalmente devido ao uso excessivo de crédito e à falta de educação financeira. Muitas pessoas não sabem gerenciar suas finanças, o que resulta em dívidas com juros altos, especialmente no crédito rotativo. Esse ciclo de endividamento dificulta a recuperação financeira e agrava a situação econômica das famílias.
+            <a>Em 2024, o endividamento das famílias brasileiras continua elevado, principalmente devido ao uso excessivo de crédito e à falta de educação financeira.
+                Muitas pessoas não sabem gerenciar suas finanças, o que resulta em dívidas com juros altos, especialmente no crédito rotativo. 
+                Esse ciclo de endividamento dificulta a recuperação financeira e agrava a situação econômica das famílias.
             </a>
         </div>
     </div>
     <div class="espacoGrafico">
         <div class = "textoGrafico"><div></div><h1>Percentual de famílias endividadas (de acordo com a CNC): </h1></div>
-    <div id="chartdiv" style = "height : 100%; width : 100%;"></div>
+    <div id="chartdiv" style = "height : 100%; width : 70%;"></div>
     </div>
     <div class="espacoOQ">
         <div class="tituloOQ">
@@ -84,7 +86,8 @@
             <a>Estamos aqui...</a>
         </div>
         <div class="infosEstamos">
-            <a class="t">Com base nessa triste realidade vivida por milhões de Brasileiros,nós surgimos, a “Save Your Money”,venho com a proposta de melhorar a vida financeira de milhares de pessoas,com profissionais e tecnologia de ponta,para ajudar você e seu dinheiro.</a>
+            <a class="t">Com base nessa triste realidade vivida por milhões de Brasileiros,nós surgimos, a “Save Your Money”,venho com a proposta de melhorar a vida
+                financeira de milhares de pessoas,com profissionais e tecnologia de ponta,para ajudar você e seu dinheiro.</a>
             <div class="btnEstamos">
                 <a href="../PAGES/selecao.php">Preciso de ajuda <i class="fa-solid fa-arrow-right"></i></a>
         </div>
@@ -138,128 +141,202 @@
             </div>
         </div>
     </div>
+    <div class="espacoPerfil">
+        <div class="conteudoPerfil">
+            <div class="fotoPerfil">
+                <img src="../PICS/imgsSobreNos/img_quebraCab" alt="">
+            </div>
+            <div class="espacoTextoPerfil">
+                <div class="tituloPerfil">
+                    <a>Perfil da</a>
+                    <a class="sepPerfil">Empresa</a>
+                </div>
+                <div class="textoPerfil">
+                    <a>Nós da Save ,acima de tudo queremos ajudar nossos clientes, lidar com dinheiro não é tarefa fácil, focamos em experiência, valorizar o trabalho, 
+                        e temos um compromisso com quem procura nossos serviços, 
+                        nosso  meta é ser uma das maiores consultora financeira do pais, sendo ligada principalmente com o público de baixa renda.</a>
+                </div>
+                <div class="espacoBtnPerfil">
+                    <div class="btnPerfil">
+                        <a href="#">Faça Parte <i class="fa-solid fa-chevron-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="espacoServicos">
+        <div class="esquerdaServicos">
+            <div class="tituloServicos">
+                <a>Nossos</a>
+                <a>Serviços</a>
+            </div>
+            <div class="fotoServicos">
+                <img src="../PICS/imgsSobreNos/img_Servicos" alt="">
+            </div>
+        </div>
+        <div class="direitaServicos">
+            <div class="daleServicos">
+                <div class="esquerdaDale">
+                    <a>01</a>
+                </div>
+                <div class="direitaDale">
+                    <div class="cimaDale">
+                        <a>Cambio Atual</a>
+                    </div>
+                    <div class="baixoDale">
+                        <a>Temos aplicação mostrando o cambio de moedas do mundo inteiro.</a>
+                    </div>
+                </div>
+            </div>
+            <div class="daleServicos">
+                <div class="esquerdaDale">
+                    <a>02</a>
+                </div>
+                <div class="direitaDale">
+                    <div class="cimaDale">
+                        <a>Bolsa de Valores</a>
+                    </div>
+                    <div class="baixoDale">
+                        <a>Criamos um programa para monitora mais de 25 ações na bolsa.</a>
+                    </div>
+                </div>
+            </div>
+            <div class="daleServicos">
+                <div class="esquerdaDale">
+                    <a class="txt3">03</a>
+                </div>
+                <div class="direitaDale">
+                    <div class="cimaDale">
+                        <a>DashBoard</a>
+                    </div>
+                    <div class="baixoDale">
+                        <a>Após coletar todas as informações financeira do usuario criamos uma DashBoard pessoa,mostrandos todos estes dados.</a>
+                    </div>
+                </div>
+            </div>
+    </div>
 </body>
 <script>
-am5.ready(function() {
+    am5.ready(function() {
 
-// Create root element
-// https://www.amcharts.com/docs/v5/getting-started/#Root_element
-var root = am5.Root.new("chartdiv");
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new("chartdiv");
 
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
-root.setThemes([
-  am5themes_Animated.new(root)
-]);
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+    am5themes_Animated.new(root)
+    ]);
 
-// Create chart
-// https://www.amcharts.com/docs/v5/charts/xy-chart/
-var chart = root.container.children.push(am5xy.XYChart.new(root, {
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/
+    var chart = root.container.children.push(am5xy.XYChart.new(root, {
 
- 
-  
-  paddingLeft:0,
-  paddingRight:1
-}));
+    
+    
+    paddingLeft:0,
+    paddingRight:1
+    }));
 
-// Add cursor
-// https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
-var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
-cursor.lineY.set("visible", false);
-
-
-// Create axes
-// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-var xRenderer = am5xy.AxisRendererX.new(root, { 
-  minGridDistance: 30, 
-  minorGridEnabled: true
-});
-
-xRenderer.labels.template.setAll({
-  rotation: 0, // Define a rotação como 0 para deixar os rótulos na horizontal
-  centerY: am5.p50,
-  centerX: am5.p100,
-  paddingRight: 15
-});
+    // Add cursor
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+    var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+    cursor.lineY.set("visible", false);
 
 
-xRenderer.grid.template.setAll({
-  location: 1
-})
+    // Create axes
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+    var xRenderer = am5xy.AxisRendererX.new(root, { 
+    minGridDistance: 30, 
+    minorGridEnabled: true
+    });
 
-var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
-  maxDeviation: 0.3,
-  categoryField: "country",
-  renderer: xRenderer,
-  tooltip: am5.Tooltip.new(root, {})
-}));
-
-var yRenderer = am5xy.AxisRendererY.new(root, {
-  strokeOpacity: 0.1
-})
-
-var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-  maxDeviation: 0.3,
-  renderer: yRenderer,
-  min: 0,  // Valor mínimo do eixo Y
-  max: 100   //
-}));
-
-// Create series
-// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-var series = chart.series.push(am5xy.ColumnSeries.new(root, {
-  name: "Series 1",
-  xAxis: xAxis,
-  yAxis: yAxis,
-  valueYField: "value",
-  sequencedInterpolation: true,
-  categoryXField: "country",
-  tooltip: am5.Tooltip.new(root, {
-    labelText: "{valueY}"
-  })
-}));
-
-series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5, strokeOpacity: 0 });
-series.columns.template.adapters.add("fill", function (fill, target) {
-  return "#ff6d00";  // Define a cor das colunas para #ff6d00
-});
-
-series.columns.template.adapters.add("stroke", function (stroke, target) {
-  return "#ff6d00";  // Define a cor do contorno das colunas para #ff6d00
-});
+    xRenderer.labels.template.setAll({
+    rotation: 0, // Define a rotação como 0 para deixar os rótulos na horizontal
+    centerY: am5.p50,
+    centerX: am5.p100,
+    paddingRight: 15
+    });
 
 
+    xRenderer.grid.template.setAll({
+    location: 1
+    })
 
-// Set data
-var data = [{
-  country: "2019",
-  value: 60
-}, {
-  country: "2020",
-  value: 67
-}, {
-  country: "2021",
-  value: 71
-}, {
-  country: "2022",
-  value: 78
-}, {
-  country: "2023",
-  value: 81
-}, {
-  country: "2024",
-  value: 80
-}];
+    var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+    maxDeviation: 0.3,
+    categoryField: "country",
+    renderer: xRenderer,
+    tooltip: am5.Tooltip.new(root, {})
+    }));
 
-xAxis.data.setAll(data);
-series.data.setAll(data);
+    var yRenderer = am5xy.AxisRendererY.new(root, {
+    strokeOpacity: 0.1
+    })
+
+    var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+    maxDeviation: 0.3,
+    renderer: yRenderer,
+    min: 0,  // Valor mínimo do eixo Y
+    max: 100   //
+    }));
+
+    // Create series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+    var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+    name: "Series 1",
+    xAxis: xAxis,
+    yAxis: yAxis,
+    valueYField: "value",
+    sequencedInterpolation: true,
+    categoryXField: "country",
+    tooltip: am5.Tooltip.new(root, {
+        labelText: "{valueY}"
+    })
+    }));
+
+    series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5, strokeOpacity: 0 });
+    series.columns.template.adapters.add("fill", function (fill, target) {
+    return "#ff6d00";  // Define a cor das colunas para #ff6d00
+    });
+
+    series.columns.template.adapters.add("stroke", function (stroke, target) {
+    return "#ff6d00";  // Define a cor do contorno das colunas para #ff6d00
+    });
 
 
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/
-series.appear(1000);
-chart.appear(1000, 100);
 
-}); // end am5.ready()
+    // Set data
+    var data = [{
+    country: "2019",
+    value: 60
+    }, {
+    country: "2020",
+    value: 67
+    }, {
+    country: "2021",
+    value: 71
+    }, {
+    country: "2022",
+    value: 78
+    }, {
+    country: "2023",
+    value: 81
+    }, {
+    country: "2024",
+    value: 80
+    }];
+
+    xAxis.data.setAll(data);
+    series.data.setAll(data);
+
+
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/
+    series.appear(1000);
+    chart.appear(1000, 100);
+
+    }); // end am5.ready()
 </script>
 </html>
